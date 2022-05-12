427dca1e853e73ce2a8eee3bb15ff9d779d98a14
MetteHejberg <89727072+MetteHejberg@users.noreply.github.com>
2022-05-12 14:44:00 +0200

    Update mutual_information.py

--- src/mutual_information.py
+++ src/mutual_information.py
@@ -55,44 +55,68 @@ def collocate(term, doc):
 
 # find the frequencies of the collocates
 def freq(counter, collocates, doc):
+    # define a counter
     A = counter
+    # define an empty list
     B = []
-    # for every collocate in the list
+    # for every collocate in the list in the range from 0 to the length of the collocate list
     for i in range(0, len(collocates)-1):
         # B_words are the collocates
         B_words = str(collocates[i])
+        # define another counter
         new_counter = 0
+        # for every token in the document
         for token in doc:
+            # if the token is the search term
             if token.text == B_words:
+                # append it to the counter
                 new_counter += 1
+        # append to the list      
         B.append(new_counter)
+    # define a new list
     AB = []
+    # for every collocate in the list in the range from 0 to the length of the collocate list
     for i in range(0, len(collocates)-1):
+        # define another counter
         another_counter = 0
+        # for every token in the collocate list
         for token in collocates:
+            # if the token is the search term
             if token.text == B_words:
+                # append it to the counter
                 another_counter += 1
+        # append to the list
         AB.append(another_counter)
     return A, B, AB
 
 # calculate mutual information with the following formula:
 # MI = log((AB*sizeCorpus)/(A*B*span))/log(2)
 def mutual_information(doc, collocates, AB, A, B):
-    text_length = len(doc) # corpus size
+    # get the length of the document = corpus size
+    text_length = len(doc)
+    # define a span
     span = 10
+    # define an empty list
     MI = []
+    # for every token in the range between 0 and the length of the collocate list
     for i in range(0, len(collocates)-1):
+        # get the mutual information score
         MI1 = math.log((AB[i] * text_length)/(A * B[i] * span))/math.log(2)
+        # and append it to the list
         MI.append(MI1)
     return MI
         
 # save results as csv 
 def save_results(collocates, B, AB, MI, csv_name):
+    # create list
     list_context = list(zip(collocates, B, AB, MI))
+    # make that list into a dataframe
     dframe = pd.DataFrame(list_context, columns = ['words', 'B', 'AB', 'MI'])
+    # round to 2 decimals
     dframe = dframe.round(decimals = 2)
+    # save the csv with a user-defined name
     dframe.to_csv(os.path.join("out", csv_name))
-    return dframe
+    
     
 def parse_args():
     # initialize argparse
@@ -110,7 +134,7 @@ def main():
     search_term, counter, collocates = collocate(args["term"], doc)
     A, B, AB = freq(counter, collocates, doc)
     MI = mutual_information(doc, collocates, AB, A, B)
-    dframe = save_results(collocates, B, AB, MI, args["csv_name"])
+    save_results(collocates, B, AB, MI, args["csv_name"])
 
 if __name__ == "__main__":
     main()
