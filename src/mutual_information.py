f25630dc054dd39b577a4aff385c0ffbc3e67a5b
MetteHejberg <89727072+MetteHejberg@users.noreply.github.com>
2022-05-12 13:58:30 +0200

    Update mutual_information.py

--- src/mutual_information.py
+++ src/mutual_information.py
@@ -6,33 +6,51 @@ import math
 import pandas as pd
 import argparse 
 
+# let's load the data
 def load_data(name):
+    # initiate spacy model
     nlp = spacy.load("en_core_web_sm")
     filepath = os.path.join(
     "..", "CDS-LANG", "100_english_novels", "corpus", name)
     file_object = open(filepath, "r")
     text = file_object.read()
-    text = text.lower() # make everything lower case
-    text = re.sub("\W+", " ", text) # remove unwanted characters
-    doc = nlp(text) # tokenize the text
+     # make everything lower case
+    text = text.lower()
+    # remove unwanted characters
+    text = re.sub("\W+", " ", text)
+    # tokenize the text
+    doc = nlp(text)
     return doc
 
+# create a function that finds that collocates of the user-defined search term
 def collocate(term, doc):
     # count the amount of time the user-defined search_term appears in the text
     search_term = term
+    # create counter
     counter = 0
+    # for every token in the document
     for token in doc:
+        # if the token is the search term
         if token.text == search_term:
+            # add 1 to the counter 
             counter += 1
     # get the collocates
+    # create an empty list
     collocates = []
+    # for every token in the document
     for token in doc:
+        # if the token is the search term
         if token.text == search_term:
+            # get 5 tokens before
             before = token.i - 5
+            # get 5 tokens after
             after = token.i + 5
+            # for every token in that range
             for i in range(before, after):
-                           if str(doc[i]) != search_term:
-                               collocates.append(doc[i])
+                # if the text is not the search term (we don't want to include the search itself)
+                if str(doc[i]) != search_term:
+                    # append the text to the list
+                    collocates.append(doc[i])
     return search_term, counter, collocates
 
 # find the frequencies of the collocates
