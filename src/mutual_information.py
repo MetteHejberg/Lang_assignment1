d01b3b924fc37a5386b87c75934dfcb6ac3f891a
MetteHejberg <89727072+MetteHejberg@users.noreply.github.com>
2022-05-20 09:28:02 +0200

    Update mutual_information.py

--- src/mutual_information.py
+++ src/mutual_information.py
@@ -1,145 +1,135 @@
-# load libraries
 import os
 import re
 import spacy
 import math
 import pandas as pd
-import argparse 
+import argparse
 
-# let's load the data
 def load_data(name):
-    # initiate spacy model
-    nlp = spacy.load("en_core_web_sm")
-    filepath = os.path.join(
-    "..", "CDS-LANG", "100_english_novels", "corpus", name)
+    filepath = os.path.join("..", "CDS-LANG", "100_english_novels", "corpus", name)
     file_object = open(filepath, "r")
     text = file_object.read()
-     # make everything lower case
-    text = text.lower()
-    # remove unwanted characters
-    text = re.sub("\W+", " ", text)
-    # tokenize the text
-    doc = nlp(text)
-    return doc
+    return text
 
-# create a function that finds that collocates of the user-defined search term
-def collocate(term, doc):
+def load_directory(directory): # I got this function from Agnes
+    path = os.path.join("..", "CDS-LANG", "100_english_novels", directory)
+    file_list = os.listdir(path)
+    return path, file_list
+
+def collocate(term, text):
     # count the amount of time the user-defined search_term appears in the text
     search_term = term
-    # create counter
     counter = 0
-    # for every token in the document
+    nlp = spacy.load("en_core_web_sm")
+    nlp.max_length = 1500000
+    doc = nlp(text)
     for token in doc:
-        # if the token is the search term
         if token.text == search_term:
-            # add 1 to the counter 
             counter += 1
     # get the collocates
-    # create an empty list
     collocates = []
-    # for every token in the document
     for token in doc:
-        # if the token is the search term
         if token.text == search_term:
-            # get 5 tokens before
             before = token.i - 5
-            # get 5 tokens after
             after = token.i + 5
-            # for every token in that range
             for i in range(before, after):
-                # if the text is not the search term (we don't want to include the search itself)
                 if str(doc[i]) != search_term:
-                    # append the text to the list
                     collocates.append(doc[i])
-    return search_term, counter, collocates
+    return search_term, counter, doc, collocates
 
 # find the frequencies of the collocates
 def freq(counter, collocates, doc):
-    # define a counter
     A = counter
-    # define an empty list
     B = []
-    # for every collocate in the list in the range from 0 to the length of the collocate list
+    # for every collocate in the list
     for i in range(0, len(collocates)-1):
         # B_words are the collocates
         B_words = str(collocates[i])
-        # define another counter
         new_counter = 0
-        # for every token in the document
         for token in doc:
-            # if the token is the search term
             if token.text == B_words:
-                # append it to the counter
                 new_counter += 1
-        # append to the list      
         B.append(new_counter)
-    # define a new list
+    
     AB = []
-    # for every collocate in the list in the range from 0 to the length of the collocate list
-    for i in range(0, len(collocates)-1):
-        # define another counter
-        another_counter = 0
-        # for every token in the collocate list
+    # for every collocate in the text 
+    for i in range(0, len(collocates)-1): 
+        # define B_words which is the collocates 
+        B_words = str(collocates[i])
+        # and define a counter 
+        counter4 = 0 
+        # for every token in the list 
         for token in collocates:
-            # if the token is the search term
+            # if the token is a collocate 
             if token.text == B_words:
-                # append it to the counter
-                another_counter += 1
-        # append to the list
-        AB.append(another_counter)
+                # count it 
+                counter4 += 1
+                 # and append to the empty list         
+        AB.append(counter4)
     return A, B, AB
 
 # calculate mutual information with the following formula:
 # MI = log((AB*sizeCorpus)/(A*B*span))/log(2)
 def mutual_information(doc, collocates, AB, A, B):
-    # get the length of the document = corpus size
-    text_length = len(doc)
-    # define a span
+    text_length = len(doc) # corpus size
     span = 10
-    # define an empty list
     MI = []
-    # for every token in the range between 0 and the length of the collocate list
     for i in range(0, len(collocates)-1):
-        # get the mutual information score
         MI1 = math.log((AB[i] * text_length)/(A * B[i] * span))/math.log(2)
-        # and append it to the list
         MI.append(MI1)
     return MI
         
 # save results as csv 
-def save_results(collocates, B, AB, MI, csv_name):
-    # create list
+def save_results(collocates, B, AB, MI, name):
     list_context = list(zip(collocates, B, AB, MI))
-    # make that list into a dataframe
-    dframe = pd.DataFrame(list_context, columns = ["words", "B", "AB", "MI"]).set_index("words")
-    # round to 2 decimals
+    dframe = pd.DataFrame(list_context, columns = ['Word', 'Frequency of word', 'Frequency as collocate', 'Mutual Information']).set_index("Word")
     dframe = dframe.round(decimals = 2)
-    # save the csv with a user-defined name
-    dframe.to_csv(os.path.join("out", csv_name))
-    
+    outpath = os.path.join("out", f"collocation_{name[0:name.index(f'.')]}.csv")
+    dframe.to_csv(outpath)
     
 def parse_args():
     # initialize argparse
     ap = argparse.ArgumentParser()
-    ap.add_argument("-n", "--name", required=True, help="the name of the file to load")
+    # add command line parameters 
+    ap.add_argument("-f", "--filename", required=False, help="The filename to use")
+    ap.add_argument("-d", "--directory", required=False, help="The directory to use")
     ap.add_argument("-t", "--term", required=True, help="the user-defined search term")
-    ap.add_argument("-c", "--csv_name", required=True, help="the name of the csv file to save")
     args = vars(ap.parse_args())
-    return args
+    # return list og arguments
+    return args   
 
 # let's run the code
 def main():
-    args = parse_args()
-    doc = load_data(args["name"])
-    search_term, counter, collocates = collocate(args["term"], doc)
-    A, B, AB = freq(counter, collocates, doc)
-    MI = mutual_information(doc, collocates, AB, A, B)
-    save_results(collocates, B, AB, MI, args["csv_name"])
+    #args = parse_args()
+    #doc = load_data(args["name"])
+    #search_term, counter, collocates = collocate(args["term"], doc)
+    #A, B, AB = freq(counter, collocates, doc)
+    #MI = mutual_information(doc, collocates, AB, A, B)
+    #dframe = save_results(collocates, B, AB, MI, args["csv_name"])
 
+    args = parse_args()
+    if args["filename"] is not None and args["filename"].endswith(".txt"):
+        text = load_data(args["filename"])
+        search_term, counter, doc, collocates = collocate(args["term"], text)
+        A, B, AB = freq(counter, collocates, doc)
+        MI = mutual_information(doc, collocates, AB, A, B)
+        save_results(collocates, B, AB, MI, args["filename"])
+    elif args["directory"] is not None:
+        results = load_directory(args["directory"])
+        for filename in results[1]:
+            if filename.endswith(".txt"):
+                path = f"{results[0]}/{filename}"
+                print(f"[INFO]: Creating {filename} output")
+                text = load_data(filename)
+                search_term, counter, doc, collocates = collocate(args["term"], text)
+                A, B, AB = freq(counter, collocates, doc)
+                MI = mutual_information(doc, collocates, AB, A, B)
+                save_results(collocates, B, AB, MI, filename)
+            else:
+                pass
+    else:
+        pass    
+    
 if __name__ == "__main__":
     main()
     
-
-    
-              
-                           
